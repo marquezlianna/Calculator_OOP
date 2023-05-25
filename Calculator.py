1f5cb080a3e1917011c58fc77684314eb37c9aa3
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-05-26 01:54:21 +0800

    create a class named Calculator

---
+++ Calculator.py
@@ -0,0 +1,9 @@
+# create a class named Calculator
+class Calculator:
+    def __init__(self):
+        self.num1 = None
+        self.num2 = None
+        self.operation = None
+        self.ans = None
+     
+    
