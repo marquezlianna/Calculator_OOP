f6044e0019a7e562cd20fec88c165b734ae707df
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-05-26 02:03:34 +0800

    Perform the operation

--- Calculator.py
+++ Calculator.py
@@ -22,5 +22,25 @@ class Calculator:
     # Ask user to choose operation
     def get_operation(self):
         self.operation = input("Please enter an operation: +-*/ ")
+        
+    # Perform the operation    
+    def perform_operation(self):
+        if self.operation == "+":
+            self.ans = self.num1 + self.num2
+        elif self.operation == "-":
+            self.ans = self.num1 - self.num2
+        elif self.operation == "*":
+            self.ans = self.num1 * self.num2
+        elif self.operation == "/":
+            try:
+                self.ans = self.num1 / self.num2
+            except ZeroDivisionError as error:
+                print("Invalid equation\n")
+                print(error)
+                print("\nTry again!")
+                return False
+        else:
+            self.ans = "Invalid operation"
+        return True
      
     
