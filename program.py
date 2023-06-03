fa46d25bc776e18f63d68a0b2176ec7ba36b9dbf
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-06-03 17:00:03 +0800

    Update program.py

--- program.py
+++ program.py
@@ -1,10 +1,13 @@
 from Calculator import Calculator
 from user_interface import UserInterface
+from inherited_calculator import InheritedCalculator
 
 def main():
     calculator = Calculator()
     user_interface = UserInterface(calculator)
+    inherited_calculator = InheritedCalculator()
     user_interface.run()
-
+    inherited_calculator()
+    
 if __name__ == "__main__":
     main()
